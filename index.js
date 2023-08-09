c89242872a912562f8ea76b2dfc12b9e29230df5
COOOLFANS <COOLFANS1023@Gmail.com>
2023-08-09 17:41:37 +0800

    None
    None

---
+++ index.js
@@ -0,0 +1,44 @@
+// main.js
+
+// electron 模块可以用来控制应用的生命周期和创建原生浏览窗口
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  // 创建浏览窗口
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // 加载 index.html
+  mainWindow.loadFile('index.html')
+
+  // 打开开发工具
+  // mainWindow.webContents.openDevTools()
+}
+
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // 在 macOS 系统内, 如果没有已开启的应用窗口
+    // 点击托盘图标时通常会重新创建一个新窗口
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此, 通常
+// 对应用程序和它们的菜单栏来说应该时刻保持激活状态, 
+// 直到用户使用 Cmd + Q 明确退出
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
